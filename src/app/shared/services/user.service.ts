66af979339f3f0dabdfb2f05cbef1d35c898539c
zamyslov@gmail.com <194156567>
2018-08-10 13:46:16 +0300

    lesson 96

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -3,18 +3,19 @@ import {Injectable} from '@angular/core';
 import {map} from 'rxjs/operators';
 import {Observable} from 'rxjs';
 import {User} from '../models/user.module';
+import {BaseApi} from '../core/base-api';
 
 @Injectable()
-export class UserService {
-  constructor(private http: HttpClient) {
+export class UserService extends BaseApi {
+  constructor(public http: HttpClient) {
+    super(http);
   }
 
   getUserByEmail(email: string): Observable<any> {
-    return this.http.get(`http://localhost:3000/users?email=${email}`)
-      .pipe(map((user: User[]) => user[0]? user[0]: undefined));
+    return this.get(`users?email=${email}`).pipe(map((user: User[]) => user[0] ? user[0] : undefined));
   }
 
   createNewUser(user: User): Observable<any> {
-    return this.http.post('http://localhost:3000/users', user);
+    return this.post('users', user);
   }
 }
--- src/app/system/shared/services/bill.service.ts
+++ src/app/system/shared/services/bill.service.ts
@@ -1,28 +1,20 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
+import {BaseApi} from '../../../shared/core/base-api';
 
 @Injectable()
-export class BillService {
-  constructor(private http: HttpClient) {
+export class BillService extends BaseApi {
+  constructor(public http: HttpClient) {
+    super(http);
   }
 
   getBill(): Observable<any> {
-    return this.http.get('http://localhost:3000/bill');
-      // .pipe(map((response: Response) => {
-      //   response.json();
-      // }));
+    return this.get('bill');
   }
 
   getCurrency(base: string = 'UAH'): Observable<any> {
     return this.http.get(`http://www.floatrates.com/daily/${base}.json`);
-    // return this.http.get(`https://data.fixer.io/api/latest
-    // ?access_key=ee60ebb17b02ebe21e8895bac4e5741d&base=${base}`);
-      // .pipe(map((response: Response) => {
-      //   response.json();
-      // }));
-
   }
 
 }
