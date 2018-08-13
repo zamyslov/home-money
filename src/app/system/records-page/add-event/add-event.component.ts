6f70911b055e90ac6ff285ea91c64fb063adf257
zamyslov@gmail.com <194156567>
2018-08-13 16:34:28 +0300

    lesson 102

--- src/app/system/records-page/add-event/add-event.component.ts
+++ src/app/system/records-page/add-event/add-event.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Category} from '../../shared/models/category.model';
+import {NgForm} from '@angular/forms';
+import {WFMEvent} from '../../shared/models/event.model';
+import * as moment from 'moment'
 
 @Component({
   selector: 'wfm-add-event',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-event.component.scss']
 })
 export class AddEventComponent implements OnInit {
+  @Input() categories: Category[] = [];
+  types = [
+    {type: 'income', label: 'Доход'},
+    {type: 'outcome', label: 'Расход'}
+  ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: NgForm) {
+    let {amount, description, category, type} = form.value;
+    if (amount < 0) amount *= -1;
+
+    const event = new WFMEvent(type, amount, category, moment().format('DD.MM.YYYY HH:mm:ss'), description);
+
   }
 
 }
