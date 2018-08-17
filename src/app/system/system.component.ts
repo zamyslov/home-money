9aa7f33d223e737ccfe1c83dffe6b9680b10ea59
zamyslov@gmail.com <194156567>
2018-08-17 17:43:28 +0300

    lesson 116

--- src/app/system/system.component.ts
+++ src/app/system/system.component.ts
@@ -1,9 +1,12 @@
-import {Component} from '@angular/core';
+import {Component, HostBinding} from '@angular/core';
+import {fadeStateTrigger} from '../shared/animations/fade.animation';
 
 @Component({
   selector: 'wfm-system',
-  templateUrl: './system.component.html'
+  templateUrl: './system.component.html',
+  animations: [fadeStateTrigger]
 })
 export class SystemComponent {
+  @HostBinding('@fade') a = true;
 
 }
