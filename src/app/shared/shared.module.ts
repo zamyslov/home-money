837c395ec68c32b7aee0908506fb12b1f21eed8f
zamyslov@gmail.com <194156567>
2018-08-17 17:29:57 +0300

    lesson 115

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {NgxChartsModule} from '@swimlane/ngx-charts';
+import {LoaderComponent} from './components/loader/loader.component';
 
 @NgModule({
   imports: [FormsModule, ReactiveFormsModule, NgxChartsModule],
-  exports: [FormsModule, ReactiveFormsModule, NgxChartsModule],
+  exports: [FormsModule, ReactiveFormsModule, NgxChartsModule, LoaderComponent],
+  declarations: [LoaderComponent]
 })
 export class SharedModule {
 
